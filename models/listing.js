428f7559945a1a3ca75b18603ba8d57fb60b9c59
Kshitiz Singh Sengar <kshitizsengar089@gmail.com>
2026-07-21 21:41:37 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose= require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema= new Schema({//Schema:blueprint
+    title:{
+        type:String,
+        required:true,
+    },
+    description: String,
+    image:{
+        filename: {
+        type: String,
+        default: "listingimage",
+        },
+        url: {
+         type:String,
+         default:"https://i.pinimg.com/originals/57/e4/03/57e40361d7486e564ae934006094c498.jpg",
+         set:(v)=> v==="" ? "https://i.pinimg.com/originals/57/e4/03/57e40361d7486e564ae934006094c498.jpg":v,
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
