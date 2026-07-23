2c2d14793bf6807387148fb31828a08f66b1fffd
Kshitiz Singh Sengar <kshitizsengar089@gmail.com>
2026-07-23 23:27:58 +0530

    crud operations completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/WanderLust"
 
@@ -18,7 +19,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
-
+app.engine('ejs', ejsMate);
 
 
 app.listen(8080,()=>{
@@ -80,7 +81,7 @@ app.get("/listings/:id/edit",async(req,res)=>{
 app.put("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     console.log(req.body.listing);
-    await  Listing.findByIdAndUpdate(id, req.body.listings);
+    await  Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
 })
 //Delete Route
