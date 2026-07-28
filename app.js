5998e82cce986e8ab6b1ef7a33fcd117b319dc68
Kshitiz Singh Sengar <kshitizsengar089@gmail.com>
2026-07-28 07:12:55 +0530

    navbar completed

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.listen(8080,()=>{
